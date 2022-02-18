666ad661cf5a788279a1b7b5385f40df35c225d9
Akhil Jaisur <akhiljaisur19@gmail.com>
2022-02-18 12:01:35 +0530

    40%

---
+++ Dom Manupilation/js.js
@@ -0,0 +1,9 @@
+/** @format */
+'use strict';
+// console.log(document.querySelector('h1').textContent);
+// document.querySelector('h1').textContent = 'I am practising it';
+
+document.querySelector('.btn').addEventListener('click', function () {
+  console.log('HI i am from Js');
+  alert('You have pressed an OnClick event which is triggered when click ');
+});
